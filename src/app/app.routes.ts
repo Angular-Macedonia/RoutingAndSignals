34f6932bc0b201fea6375ea11a2fd0aacad20f15
Kiril Zafirov <kirilzafirov@Kirils-MBP.home>
2024-09-13 10:49:57 +0200

    setup base service with base url and some steps for the routig improvements

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,16 @@ import { canViewGuard } from './guards/canView.guard';
 import { isLoggedInUserGuard } from './guards/loggedIn.guard';
 
 export const routes: Routes = [
-    
+    //REDIRECTS
+    //NESTED routes
+    // Short vs Long Syntax
+    // not found page 404
+    // wildcard route
     {
         path: 'page-one',
         //short syntax
         loadComponent: () => import('./pages/page-one.component'),
+        title: 'Page One',
         canActivate: [canViewGuard()]
     },
     {
